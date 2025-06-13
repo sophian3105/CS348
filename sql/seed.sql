d3179ff543a446f729e84ee9f66ef5c2e323b28b
elsieliii <yli.elsie@gmail.com>
2025-06-13 12:33:23 -0400

    change to seed uuid

--- sql/seed.sql
+++ sql/seed.sql
@@ -1,37 +1,46 @@
 USE cs348;
 
 -- 1. USERS
+SET @uuid_u1 = UUID();
+SET @uuid_u2 = UUID();
+
 INSERT INTO users (user_id, email, full_name, created_at, birth_date)
 VALUES 
-('u1-uuid', 'elsie@example.com', 'Elsie Li', '2025-06-01', '2000-01-01'),
-('u2-uuid', 'jeff@example.com', 'Jeff Liu', '2025-06-02', '1999-12-31');
+(@uuid_u1, 'elsie@example.com', 'Elsie Li', '2025-06-01', '2000-01-01'),
+(@uuid_u2, 'jeff@example.com', 'Jeff Liu', '2025-06-02', '1999-12-31');
 
 -- 2. POLICE REPORTS
+SET @uuid_p1 = UUID();
+SET @uuid_p2 = UUID();
+
 INSERT INTO policeReports (r_id, assault_type, occurence_date, reported_date, division)
 VALUES 
-('p1-uuid', 'Physical Assault', '2025-05-10', '2025-05-11', 'Downtown'),
-('p2-uuid', 'Theft in Brampton', '2025-04-20', '2025-04-22', 'Peel');
+(@uuid_p1, 'Physical Assault', '2025-05-10', '2025-05-11', 'Downtown'),
+(@uuid_p2, 'Theft in Brampton', '2025-04-20', '2025-04-22', 'Peel');
 
 -- 3. USER REPORTS
+SET @uuid_ur1 = UUID();
+SET @uuid_ur2 = UUID();
+
 INSERT INTO userReports (r_id, assault_type, occurence_date, reported_date, user_id)
 VALUES 
-('u1-report', 'Verbal Harassment', '2025-06-05', '2025-06-06', 'u1-uuid'),
-('u2-report', 'Brampton Robbery', '2025-06-03', '2025-06-04', 'u2-uuid');
+(@uuid_ur1, 'Verbal Harassment', '2025-06-05', '2025-06-06', @uuid_u1),
+(@uuid_ur2, 'Brampton Robbery', '2025-06-03', '2025-06-04', @uuid_u2);
 
 -- 4. POLICE LOCATION
 INSERT INTO policeLocation (r_id, longitude, latitude, neighborhood, location_type, premise_type)
 VALUES
-('p1-uuid', -79.38, 43.65, 'Entertainment District', 'Street', 'Public'),
-('p2-uuid', -79.76, 43.68, 'Brampton', 'Transit Station', 'Public');
+(@uuid_p1, -79.38, 43.65, 'Entertainment District', 'Street', 'Public'),
+(@uuid_p2, -79.76, 43.68, 'Brampton', 'Transit Station', 'Public');
 
 -- 5. USER LOCATION
 INSERT INTO userLocation (r_id, longitude, latitude, neighborhood, location_type, premise_type)
 VALUES
-('u1-report', -79.40, 43.66, 'Kensington Market', 'Sidewalk', 'Public'),
-('u2-report', -79.75, 43.70, 'Brampton', 'Park', 'Public');
+(@uuid_ur1, -79.40, 43.66, 'Kensington Market', 'Sidewalk', 'Public'),
+(@uuid_ur2, -79.75, 43.70, 'Brampton', 'Park', 'Public');
 
 -- 6. ASSAULT COMMENTS
 INSERT INTO assaultComments (user_id, r_id, created_at, comments)
 VALUES
-('u1-uuid', 'u2-report', '2025-06-10', 'Sorry this happened to you!'),
-('u2-uuid', 'u1-report', '2025-06-11', 'I was nearby that day too.');
+(@uuid_u1, @uuid_ur2, '2025-06-10', 'Sorry this happened to you!'),
+(@uuid_u2, @uuid_ur1, '2025-06-11', 'I was nearby that day too.');
